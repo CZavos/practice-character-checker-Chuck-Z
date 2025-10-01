8eb41c7dbacf5bc7260c629e36792bcfae2b1f39
CZavos <149090367+CZavos@users.noreply.github.com>
2025-10-01 08:35:03 -0500

    Update practice-character-checker-Chuck-Z.js

--- CZavos/practice-character-checker-Chuck-Z.js
+++ CZavos/practice-character-checker-Chuck-Z.js
@@ -1,1 +1,5 @@
-
+const input = require('readline-sync');
+let string1 = input.question("Enter a string: ");
+let index1 = input.questionInt("Enter an index number: ");
+character = string1[index1];
+console.log("The character at index " + index1 + " is: " + character + ".");
